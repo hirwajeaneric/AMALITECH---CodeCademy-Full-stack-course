c0a82c132fbda57d5b3ae1479a2e0c7919c14355
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-04-30 16:09:39 +0200

    Adding individual functions and attributes

--- 16. JavaScript Syntax, Part III/Inheritence/HospitalEmployee.js
+++ 16. JavaScript Syntax, Part III/Inheritence/HospitalEmployee.js
@@ -15,21 +15,25 @@ class HospitalEmployee {
     takeVacationDays(daysOff) {
       this._remainingVacationDays -= daysOff;
     }
-}
-
-class Nurse extends HospitalEmployee{
-    constructor(name, certifications){
-        super(name);
-        this._remainingVacationDays = 20;
-        this._certifications = certifications;
+  }
+  
+  class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+      super(name);
+      this._certifications = certifications;
+    } 
+  
+    get certifications(){
+      return this._certifications;
     }
-
-    addCertification(){
-
+  
+    addCertification(newCertification){
+      this.certifications.push(newCertification)
     }
-}
-
-const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
-
-nurseOlynyk.takeVacationDays(5);
-console.log(nurseOlynyk.remainingVacationDays);
+  }
+  
+  const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+  nurseOlynyk.takeVacationDays(5);
+  console.log(nurseOlynyk.remainingVacationDays);
+  nurseOlynyk.addCertification('Genetics');
+  console.log(nurseOlynyk.certifications);
