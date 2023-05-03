3d4cb98735532c43f747c99599c9ad55342b1075
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-05-03 10:44:05 +0200

    Library Project progress

--- 16. JavaScript Syntax, Part III/Project - Build a library/Media.js
+++ 16. JavaScript Syntax, Part III/Project - Build a library/Media.js
@@ -39,13 +39,36 @@ class Media {
 };
 
 class Book extends Media {
+    constructor(title, author, pages){
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
 
+    get author(){
+        return this._author;
+    }
+
+    get pages(){
+        return this._pages;
+    }
 }
 
 class Movie extends Media {
-    
+    constructor(title, director, runTime){
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+
+    get director(){
+        return this._director;
+    }
+
+    get runTime(){
+        return this._runTime;
+    }
 }
 
-class CD extends Media {
-    
-}
+const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544);
+console.log(historyOfEverything.author);
